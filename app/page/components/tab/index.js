64eb7b79ff5dab4ce5e61ecbddac3bc92b4d59ae
huangtubiao <1334615045@qq.com>
2016-11-04 00:10:57 +0800

    add tab component

--- app/page/components/tab/index.js
+++ app/page/components/tab/index.js
@@ -7,11 +7,20 @@ import classNames from 'classnames';
 require('./index.scss');
 
 function TabItem() {
-
+    return (
+        <li data-tab={item.label}
+            key={key}
+        >
+            <Touch data-tab={item.label} onTap={this.switchTab}>{item.text}</Touch>
+        </li>
+    )
 }
 
 function TabHighlight() {
-
+    var isActive = (props.active === LIKE_NEWS);
+    return (
+        <i className={classNames('icon-active', {'pull-right': isActive})}></i>
+    )
 }
 
 export default class Tab extends Component {
@@ -20,6 +29,17 @@ export default class Tab extends Component {
         this.state = {
             
         };
+        this.tabs = [
+            {
+                label: LATEST_NEWS,
+                text: '最新新闻'
+            },
+            {
+                label: LIKE_NEWS,
+                text: '我的收藏'
+            }
+        ];
+        this.switchTab = this.switchTab.bind(this);
     }
 
     componentWillMount() {
@@ -31,12 +51,21 @@ export default class Tab extends Component {
     }
 
     switchTab() {
-
+        let tab = parseInt(e.target.dataset.tab); // ?????
+        this.props.updateActiveTab(tab);
     }
 
     render() {
         return (
             <div id="cm-tab">
+                <div className="cm-tabs">
+                    <nav className="nav ui-border-1px">
+                        <ul className="title-list">
+                            {this.tabs.map(TabItem, this)}
+                        </ul>
+                        <TabHighlight active={this.props.tabs}/>
+                    </nav>
+                </div>
             </div>
         )
     }
